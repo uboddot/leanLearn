4bc9ef74ec857cc8d0dc2654ffc4a351097e2862
urielBrawer <uriel.brawer@students.fhnw.ch>
2026-04-20 18:19:56 +0200

    test of actual container

--- frontend/tests/example.spec.ts
+++ frontend/tests/example.spec.ts
@@ -16,3 +16,9 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('lean learn home page is present', async({ page }) => {
+  await page.goto('http://localhost:5173/'); //http://localhost:5173/
+
+  await expect(page).toHaveTitle(/frontend/);
+});
