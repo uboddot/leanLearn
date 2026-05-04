2b2a6f184bad5b8bcfcb0cfeedbd72b2d1b6ec45
urielBrawer <uriel.brawer@students.fhnw.ch>
2026-05-04 18:15:15 +0200

    cors only on /api/ route and per default empty string

--- backend/src/main/java/com/leanLearn/backend/config/CorsConfig.java
+++ backend/src/main/java/com/leanLearn/backend/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/api/**")
                 .allowedOriginPatterns(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
