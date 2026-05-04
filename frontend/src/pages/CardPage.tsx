1260b628cb48f5b620504d02b7a7cf5e81d26265
urielBrawer <uriel.brawer@students.fhnw.ch>
2026-05-04 18:07:50 +0200

    added error handling for fetch

--- frontend/src/pages/CardPage.tsx
+++ frontend/src/pages/CardPage.tsx
@@ -5,20 +5,30 @@ import type { Voci } from "../types/Voci";
 export default function CardPage() {
 
   const [words, setWords] = useState<Voci[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     async function fetchWords() {
-      const response = await getWords();
-      setWords(response);
+      try {
+        setLoading(true);
+        setError(null);
+        const response = await getWords();
+        setWords(response);
+      } catch {
+        setError("Failed to fetch words");
+      } finally {
+        setLoading(false);
+      }
     }
     fetchWords();
   }, []);
   
   return (
-      <div style={{ textAlign: 'center', fontSize: '24px', fontWeight: 'bold' }}>
-        {words ? words.map((item, index) => (
-          <div key={index}>{item.original} - {item.translation}</div>
-        )) : 'Loading...'}
-      </div>
+    <div style={{ textAlign: 'center', fontSize: '24px', fontWeight: 'bold' }}>
+      {loading ? 'Loading...' : error ? error : words.map((item, index) => (
+        <div key={index}>{item.original} - {item.translation}</div>
+      ))}
+    </div>
   )
 }
