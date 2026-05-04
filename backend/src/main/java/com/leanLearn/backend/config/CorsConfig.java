53febfbda838282b05eac4e978f6f83ac15fa195
urielBrawer <uriel.brawer@students.fhnw.ch>
2026-05-04 09:17:26 +0200

    test with new cors config

--- backend/src/main/java/com/leanLearn/backend/config/CorsConfig.java
+++ backend/src/main/java/com/leanLearn/backend/config/CorsConfig.java
@@ -1,18 +1,20 @@
 package com.leanlearn.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@Profile("dev")
 public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*")
+                .allowedOriginPatterns(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
